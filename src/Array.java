82c3167a1aefb6c1d8d1d7c9ab82b94e515a5296
llt22 <262387013@qq.com>
2018-05-18 04:20:59 +0800

    查找，删除元素相关的方法

--- src/Array.java
+++ src/Array.java
@@ -61,6 +61,58 @@ public class Array {
         insert(0, item);
     }
 
+    public int find(int item) {
+        for (int i = 0; i < size; i++) {
+            if (data[i] == item) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public boolean contains(int item) {
+        int index = find(item);
+        return index > 0;
+    }
+
+    public int remove(int index) {
+        if (index < 0 || index >= size) {
+            throw new IllegalArgumentException("remove failed,  index is out of range");
+        }
+        for (int i = index; i < size - 1; i++) {
+            data[i] = data[i + 1];
+        }
+        size--;
+        return data[index];
+    }
+
+    public int removeFirst() {
+        return remove(0);
+    }
+
+    public int removeLast() {
+        return remove(size - 1);
+    }
+
+    public boolean removeElement(int item) {
+        int index = find(item);
+        if (index != -1) {
+            remove(index);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean removeAllElements(int item) {
+        boolean isRemoved = removeElement(item);
+        boolean isRemoveAll = false;
+        while (isRemoved) {
+            isRemoved = removeElement(item);
+            isRemoveAll = true;
+        }
+        return isRemoveAll;
+    }
+
     @Override
     public String toString() {
         if (size != 0) {

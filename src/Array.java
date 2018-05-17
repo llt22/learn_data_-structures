707d26d3903895c9a38d23505ed50b699d2baa52
llt22 <262387013@qq.com>
2018-05-18 04:12:18 +0800

    格式化输出数组

--- src/Array.java
+++ src/Array.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Array {
     private int[] data;
     private int size;
@@ -9,5 +11,20 @@ public class Array {
 
     public Array() {
         this(10);
+    }
+
+    @Override
+    public String toString() {
+        if (size != 0) {
+            String arr = Arrays.toString(this.data);
+            String subArr = arr.substring(0, 3 * size - 1) + "]";
+            String s = subArr +
+                    "\ncapacity: " + data.length +
+                    "    size: " + size;
+
+            return s;
+        } else {
+            return "[ ]";
+        }
     }
 }

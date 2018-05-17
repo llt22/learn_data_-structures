c8eafafcab4da93850a98ffddaa2a5573a05a3a5
llt22 <262387013@qq.com>
2018-05-18 04:11:19 +0800

    编写构造函数

--- src/Array.java
+++ src/Array.java
@@ -1,2 +1,13 @@
 public class Array {
+    private int[] data;
+    private int size;
+
+    public Array(int capacity) {
+        this.data = new int[capacity];
+        this.size = 0;
+    }
+
+    public Array() {
+        this(10);
+    }
 }
